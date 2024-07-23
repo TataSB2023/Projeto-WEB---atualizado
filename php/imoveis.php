541c33d014d511cd9253563b378103b6b974a41c
Eduarda-SS <137419071+Eduarda-SS@users.noreply.github.com>
2024-07-23 07:48:49 -0300

    php get

--- php/imoveis.php
+++ php/imoveis.php
@@ -1,9 +1,9 @@
 <?php
-	echo "CheckIn: " .$_POST ['CheckIn']. "<br>";
-	echo "CheckOut: " .$_POST ['CheckOut']. "<br>";
-	echo "Adults: " .$_POST ['Adults']. "<br>";
-	echo "Kids: " .$_POST ['Kids']. "<br>";
-	echo "Name: " .$_POST ['Name']. "<br>";
-	echo "Email: " .$_POST ['Email']. "<br>";
-    echo "Message: " .$_POST ['Message']. "<br>";
+	echo "CheckIn: " .$_GET ['CheckIn']. "<br>";
+	echo "CheckOut: " .$_GET ['CheckOut']. "<br>";
+	echo "Adults: " .$_GET ['Adults']. "<br>";
+	echo "Kids: " .$_GET ['Kids']. "<br>";
+	echo "Name: " .$_GET ['Name']. "<br>";
+	echo "Email: " .$_GET ['Email']. "<br>";
+    echo "Message: " .$_GET ['Message']. "<br>";
 ?>
